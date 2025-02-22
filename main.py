81af1898a5ce208eba4486fe2880433972819e3a
Umit D Sami <umit.sami.usa@gmail.com>
2025-02-22 18:05:10 -0500

    5th commit

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import os
 from dotenv import load_dotenv, dotenv_values
+import toml
+
 load_dotenv()
 print(os.getenv("MY_KEY"))
 
 print("hello prj-7")
+
+with open('prj-7.config.toml', 'r') as f:
+	config = toml.load(f)
+
+#Access values from the toml config file
+print(config['server']['host'])
+print(config['server']['port'])
+print(config['database']['username'])
+
+
 
